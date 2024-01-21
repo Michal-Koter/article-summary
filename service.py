324361e2ba790f75b3fd6fe4e431ad575dac97c4
Koter Michal <s24569@pjwstk.edu.pl>
2024-01-21 22:03:28 +0100

    remove unnecessary code

--- service.py
+++ service.py
@@ -7,15 +7,6 @@ class Service:
         self.destilbart = Model("Falconsai/text_summarization")
         self.t5_small = Model("sshleifer/distilbart-cnn-12-6")
 
-    def get_summary(self, text: str):
-        result = dict()
-
-        result['bart'] = self.bart.get_summary(text)
-        result['destilbart'] = self.destilbart.get_summary(text)
-        result['t5_small'] = self.t5_small.get_summary(text)
-
-        return result
-
     def get_summary_bart(self, text: str):
         return self.bart.get_summary(text)
 
